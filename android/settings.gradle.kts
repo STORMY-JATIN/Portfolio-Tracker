298d63442007a3f9edb80b0459349f5193b1417f
Jatin Singh <jatin250306@gmail.com>
2026-05-21 00:13:29 +0530

    Permanently fix missing Google repository locally

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,2 +1,18 @@
-rootProject.name = "PortfolioTracker"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Portfolio-Tracker"
 include(":app")
